1e8b24101ed73b22d0c52975ebac4e76297985c6
Israel <israelagbo83@gmail.com>
2026-09-09 14:14:40 +0100

    My initial commit for the python lesson

---
+++ globalCode2026.py
@@ -0,0 +1,13 @@
+# Creating a list and appending something to it at the end
+names = ['John', "Paul","George", "Ringo"]
+myName = "Israel"
+names.append(myName)
+# print(names)
+
+# another Trial work
+names2 = ['John', "Paul","George", 3]
+print(type(names2[3]))
+
+# Removing something from the list
+names2.remove('John')
+print(names2)
